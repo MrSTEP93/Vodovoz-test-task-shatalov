7c43b87958ebbed32cc0151e22e52e20f50b26a5
shatalov@work <shatalov.ag@mail.ru>
2026-06-20 20:54:26 +0300

    feat: created service interfaces (#1)

---
+++ Vodovoz.Domain/Interfaces/IClientService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vodovoz.Domain.Entities;
+
+namespace Vodovoz.Domain.Interfaces
+{
+    public interface IClientService
+    {
+        Client? GetById(int id);
+        IEnumerable<Client> GetAll();
+        void Save(Client client);
+        void Delete(int id);
+    }
+}
