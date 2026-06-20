2c4b0af46b4bc615eb792794e9227a40f682e8fa
shatalov@work <shatalov.ag@mail.ru>
2026-06-20 17:03:21 +0300

    feat: add mapings (#1)

---
+++ Vodovoz.Data/Mappings/ClientMap.cs
@@ -0,0 +1,23 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Vodovoz.Domain.Entities;
+
+namespace Vodovoz.Data.Mappings
+{
+    /// <summary>
+    /// Маппинг NHibernate для модели Client
+    /// </summary>
+    public class ClientMap : ClassMap<Client>
+    {
+        public ClientMap() 
+        {
+            Table("Clients");
+            Id(x => x.Id).GeneratedBy.Identity();
+            Map(x => x.Name).Length(255).Not.Nullable();
+            Map(x => x.Inn).Length(12).Not.Nullable();
+            References(x => x.Curator).Column("CuratorId").Not.Nullable();
+        }
+    }
+}
---
+++ Vodovoz.Data/Mappings/EmployeeMap.cs
@@ -0,0 +1,23 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Vodovoz.Domain.Entities;
+
+namespace Vodovoz.Data.Mappings
+{
+    /// <summary>
+    /// Маппинг NHibernate для модели Employee
+    /// </summary>
+    public class EmployeeMap : ClassMap<Employee>
+    {
+        public EmployeeMap()
+        {
+            Table("Employees");
+            Id(x => x.Id).GeneratedBy.Identity();
+            Map(x => x.FullName).Length(255).Not.Nullable();
+            Map(x => x.Position).CustomType<int>().Not.Nullable();
+            Map(x => x.BirthDate).Not.Nullable();
+        }
+    }
+}
