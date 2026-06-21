7dc739e1a7967e7a9134b9f3f4f525f9f1f3c17e
shatalov@work <shatalov.ag@mail.ru>
2026-06-22 01:04:55 +0300

    test: created order service test class (#3)

---
+++ Vodovoz.Tests/Services/OrderServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vodovoz.Tests.Services
+{
+    namespace Vodovoz.Tests.Services
+    {
+        [TestFixture]
+        public class OrderServiceTests
+        {
+        }
+    }
+}
