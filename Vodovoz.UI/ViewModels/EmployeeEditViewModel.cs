62f801c3a49298a5651727853dcbb2851508f234
shatalov@work <shatalov.ag@mail.ru>
2026-06-23 01:41:45 +0300

    fear: brain was fucked (#5)

---
+++ Vodovoz.UI/ViewModels/EmployeeEditViewModel.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using Vodovoz.Domain.Entities;
+using Vodovoz.Domain.Enums;
+using Vodovoz.Domain.Exceptions;
+using Vodovoz.Domain.Interfaces;
+using Vodovoz.UI.Common;
+
+namespace Vodovoz.UI.ViewModels
+{
+    public class EmployeeEditViewModel : ViewModelBase
+    {
+        private readonly IEmployeeService _employeeService;
+        private readonly Employee _employee = null!;
+        private bool _isEditMode;
+        
+        public string Surname { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Patronymic { get; set; } = string.Empty;
+        public Position Position { get; set; }
+        public DateTime BirthDate { get; set; } = DateTime.Today;
+
+        public string Title => _isEditMode ? "Редактирование сотрудника" : "Добавление сотрудника";
+
+        public ICommand SaveCommand { get; private set; } = null!;
+        public ICommand CancelCommand { get; private set; } = null!;
+
+        public IEnumerable<Position> Positions => Enum.GetValues(typeof(Position)).Cast<Position>();
+
+        public EmployeeEditViewModel(Employee employee, IEmployeeService employeeService)
+        {
+            _employee = employee ?? throw new ArgumentNullException(nameof(employee));
+            _employeeService = employeeService ?? throw new ArgumentNullException(nameof(employeeService));
+            _isEditMode = true;
+
+            // Заполняем поля из сущности
+            Surname = employee.Surname;
+            Name = employee.Name;
+            Patronymic = employee.Patronymic;
+            Position = employee.Position;
+            BirthDate = employee.BirthDate;
+
+            InitializeCommands();
+        }
+
+        public EmployeeEditViewModel(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService ?? throw new ArgumentNullException(nameof(employeeService));
+            _isEditMode = false;
+            InitializeCommands();
+        }
+
+        private void InitializeCommands()
+        {
+            SaveCommand = new RelayCommand(Save, () =>
+                !string.IsNullOrWhiteSpace(Surname) && !string.IsNullOrWhiteSpace(Name));
+
+            CancelCommand = new RelayCommand(CloseWindow);
+        }
+
+        private void Save()
+        {
+            try
+            {
+                if (_isEditMode)
+                {
+                    _employee.Surname = Surname;
+                    _employee.Name = Name;
+                    _employee.Patronymic = Patronymic;
+                    _employee.Position = Position;
+                    _employee.BirthDate = BirthDate;
+
+                    _employeeService.Save(_employee);
+                }
+                else
+                {
+                    var newEmployee = new Employee
+                    {
+                        Surname = Surname,
+                        Name = Name,
+                        Patronymic = Patronymic,
+                        Position = Position,
+                        BirthDate = BirthDate
+                    };
+
+                    _employeeService.Save(newEmployee);
+                }
+
+                CloseWindow();
+            }
+            catch (BusinessRuleException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка сохранения", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Критическая ошибка: {ex}");
+                MessageBox.Show("Произошла непредвиденная ошибка", "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void CloseWindow()
+        {
+            foreach (var window in Application.Current.Windows)
+            {
+                if (window is Window w && w.DataContext == this)
+                {
+                    w.DialogResult = true;
+                    w.Close();
+                    break;
+                }
+            }
+        }
+    }
+}
--- Vodovoz.UI/ViewModels/EmployeesViewModel.cs
+++ Vodovoz.UI/ViewModels/EmployeesViewModel.cs
@@ -2,26 +2,39 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows;
+using System.Windows.Input;
 using Vodovoz.Domain.Entities;
 using Vodovoz.Domain.Interfaces;
 using Vodovoz.UI.Common;
+using Vodovoz.UI.Views;
 
 namespace Vodovoz.UI.ViewModels
 {
     public class EmployeesViewModel : ViewModelBase 
     {
         private readonly IEmployeeService _employeeService;
-        private ObservableCollection<Employee> _employees = null!;
 
+        private ObservableCollection<Employee> _employees = null!;
         public ObservableCollection<Employee> Employees
         {
             get => _employees;
             set => SetProperty(ref _employees, value);
         }
 
+        private Employee _selectedEmployee = null!;
+        public Employee SelectedEmployee
+        {
+            get => _selectedEmployee;
+            set => SetProperty(ref _selectedEmployee, value);
+        }
+
+        public ICommand AddEmployeeCommand { get; private set; }
+
         public EmployeesViewModel(IEmployeeService employeeService)
         {
             _employeeService = employeeService;
+            AddEmployeeCommand = new RelayCommand(OpenAddEmployeeWindow);
             LoadEmployees();
         }
 
@@ -29,6 +42,24 @@ namespace Vodovoz.UI.ViewModels
         {
             var list = _employeeService.GetAll();
             Employees = new ObservableCollection<Employee>(list);
+        }
+
+        private void OpenAddEmployeeWindow()
+        {
+            var editVm = new EmployeeEditViewModel(_employeeService);
+
+            var window = new EmployeeEditWindow
+            {
+                DataContext = editVm,
+                Owner = Application.Current.MainWindow
+            };
+
+            var result = window.ShowDialog();
+
+            if (result == true)
+            {
+                LoadEmployees();
+            }
         }
     }
 }
