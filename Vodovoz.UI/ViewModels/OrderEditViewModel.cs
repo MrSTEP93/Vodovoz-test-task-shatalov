60f0a01158e6fe0382858999386a2b6094b6aeb8
shatalov@work <shatalov.ag@mail.ru>
2026-06-24 01:30:52 +0300

    feat: add edit order form (#5)

---
+++ Vodovoz.UI/ViewModels/OrderEditViewModel.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Xml.Linq;
+using Vodovoz.Domain.Entities;
+using Vodovoz.Domain.Exceptions;
+using Vodovoz.Domain.Interfaces;
+using Vodovoz.UI.Common;
+
+namespace Vodovoz.UI.ViewModels
+{
+    public class OrderEditViewModel : ViewModelBase
+    {
+        private readonly IOrderService _orderService;
+        private readonly IClientService _clientService;
+        private readonly IEmployeeService _employeeService;
+        private readonly Order _order = null!;
+        private bool _isEditMode;
+
+        public IEnumerable<Employee> Employees { get; }
+        public IEnumerable<Client> Clients { get; }
+
+        public DateTime? Date { get; set; } = DateTime.Now;
+        public Decimal? Sum { get; set; } = 0.00M;
+        public Employee Employee { get; set; } = null!;
+        public Client Client { get; set; } = null!;
+
+        public int EmployeeId { get; set; } = 0;
+        public int ClientId { get; set; } = 0;
+
+        public string Title => _isEditMode ? "Редактирование заказа" : "Добавление заказа";
+
+        public ICommand SaveCommand { get; private set; } = null!;
+        public ICommand CancelCommand { get; private set; } = null!;
+
+        public OrderEditViewModel(IOrderService orderService, IEmployeeService employeeService, IClientService clientService)
+        {
+            _orderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
+            _employeeService = employeeService ?? throw new ArgumentNullException(nameof(employeeService));
+            _clientService = clientService ?? throw new ArgumentNullException(nameof(orderService));
+
+            _isEditMode = false;
+
+            Employees = _employeeService.GetAll();
+            Clients = _clientService.GetAll();
+            InitializeCommands();
+        }
+
+        public OrderEditViewModel(IOrderService orderService, IEmployeeService employeeService, 
+            IClientService clientService, Order selectedOrder)
+        {
+            _orderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
+            _employeeService = employeeService ?? throw new ArgumentNullException(nameof(employeeService));
+            _clientService = clientService ?? throw new ArgumentNullException(nameof(orderService));
+
+            _isEditMode = true;
+
+            Date = selectedOrder.Date;
+            Sum = selectedOrder.Sum;
+            Employee = selectedOrder.Employee;
+            Client = selectedOrder.Client;
+            EmployeeId = selectedOrder.Employee.Id;
+            ClientId = selectedOrder.Client.Id;
+
+            Employees = _employeeService.GetAll();
+            Clients = _clientService.GetAll();
+            InitializeCommands();
+        }
+
+        private void InitializeCommands()
+        {
+            SaveCommand = new RelayCommand(Save, () => Date.HasValue && Sum.HasValue && Sum.Value > 0);
+
+            CancelCommand = new RelayCommand(Cancel);
+        }
+
+        private void Save()
+        {
+            try
+            {
+                if (_isEditMode)
+                {
+                    _order.Date = Date!.Value;
+                    _order.Sum = Sum!.Value;
+                    _order.Employee = Employees.FirstOrDefault(e => e.Id == EmployeeId)!;
+                    _order.Client = Clients.FirstOrDefault(e => e.Id == ClientId)!;
+                    _orderService.Save(_order);
+                }
+                else
+                {
+                    var newOrder = new Order
+                    {
+                        Date = Date!.Value,
+                        Sum = Sum!.Value,
+                        Employee = Employees.FirstOrDefault(e => e.Id == EmployeeId)!,
+                        Client = Clients.FirstOrDefault(e => e.Id == ClientId)!,
+                    };
+                    _orderService.Save(newOrder);
+                }
+                CloseWindow(true);
+            }
+            catch (BusinessRuleException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка сохранения", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Критическая ошибка: {ex}");
+                MessageBox.Show("Произошла непредвиденная ошибка", "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Cancel()
+        {
+            CloseWindow(false);
+        }
+
+        private void CloseWindow(bool dialogResult)
+        {
+            foreach (var window in Application.Current.Windows)
+            {
+                if (window is Window w && w.DataContext == this)
+                {
+                    w.DialogResult = dialogResult;
+                    w.Close();
+                    break;
+                }
+            }
+        }
+    }
+}
--- Vodovoz.UI/ViewModels/OrdersViewModel.cs
+++ Vodovoz.UI/ViewModels/OrdersViewModel.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows;
 using System.Windows.Input;
 using Vodovoz.Domain.Entities;
+using Vodovoz.Domain.Exceptions;
 using Vodovoz.Domain.Interfaces;
+using Vodovoz.Services;
 using Vodovoz.UI.Common;
+using Vodovoz.UI.Views;
 
 namespace Vodovoz.UI.ViewModels
 {
     public class OrdersViewModel : ViewModelBase
     {
         private IOrderService _orderService;
+        private IEmployeeService _employeeService;
+        private IClientService _clientService;
         private ObservableCollection<Order> _orders = null!;
         public ObservableCollection<Order> Orders
         {
@@ -27,14 +33,18 @@ namespace Vodovoz.UI.ViewModels
         }
 
         public ICommand AddOrderCommand { get; private set; }
-
         public ICommand EditOrderCommand { get; private set; }
+        public ICommand DeleteOrderCommand { get; private set; }
 
-        public OrdersViewModel(IOrderService orderService)
+        public OrdersViewModel(IOrderService orderService, IEmployeeService employeeService, IClientService clientService)
         {
             _orderService = orderService;
-            //AddOrderCommand = new RelayCommand(OpenAddOrderWindow);
-            //EditOrderCommand = new RelayCommand(OpenEditOrderWindow, () => SelectedOrder != null);
+            _employeeService = employeeService;
+            _clientService = clientService;
+
+            AddOrderCommand = new RelayCommand(OpenAddOrderWindow);
+            EditOrderCommand = new RelayCommand(OpenEditOrderWindow, () => SelectedOrder != null);
+            DeleteOrderCommand = new RelayCommand(DeleteOrder, () => SelectedOrder != null);
             LoadOrders();
         }
 
@@ -42,6 +52,70 @@ namespace Vodovoz.UI.ViewModels
         {
             var list = _orderService.GetAll();
             Orders = new ObservableCollection<Order>(list);
+        }
+
+        private void OpenAddOrderWindow()
+        {
+            var editVm = new OrderEditViewModel(_orderService, _employeeService, _clientService);
+
+            var window = new OrderEditWindow
+            {
+                DataContext = editVm,
+                Owner = Application.Current.MainWindow
+            };
+
+            var result = window.ShowDialog();
+
+            if (result == true)
+            {
+                LoadOrders();
+            }
+        }
+
+        private void OpenEditOrderWindow()
+        {
+            var editVm = new OrderEditViewModel(_orderService, _employeeService, _clientService, SelectedOrder);
+
+            var window = new OrderEditWindow
+            {
+                DataContext = editVm,
+                Owner = Application.Current.MainWindow
+            };
+
+            var result = window.ShowDialog();
+
+            if (result == true)
+            {
+                LoadOrders();
+            }
+        }
+
+
+        private void DeleteOrder()
+        {
+            var result = MessageBox.Show(
+                $"Вы уверены, что хотите удалить заказ от \"{SelectedOrder.Date}\"?",
+                "Подтверждение удаления",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question);
+
+            if (result != MessageBoxResult.Yes)
+                return;
+
+            try
+            {
+                _clientService.Delete(SelectedOrder.Id);
+                LoadOrders();
+            }
+            catch (BusinessRuleException ex)
+            {
+                MessageBox.Show(ex.Message, "Невозможно удалить", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Ошибка удаления: {ex}");
+                MessageBox.Show("Произошла ошибка при удалении", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
